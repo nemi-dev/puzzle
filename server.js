f7bd9499bc5fba4870b9164431cf4bd415b37058
descart2 <n@nemi.io>
2020-06-28 14:14:25 +0900

    project structure

--- server.js
+++ server.js
@@ -1,51 +1,16 @@
 const express = require('express');
-const httpError = require('http-errors');
-
 const path = require('path');
+
 const app = express();
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'));
-});
+router.use(express.static(path.resolve(__dirname, 'dist'), {
+	extensions : ['html']
+}))
 
-router.get('/style.css', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'style.css'));
-});
-
-
-router.get('/puzzleset.json', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'puzzleset.json'));
-});
-
-router.get('/app.js', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'app.js'));
-});
-
-router.get('/hit.wav', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'hit.wav'));
-});
-
-router.get('/settings.svg', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'settings.svg'));
-});
-
-router.get('/info.svg', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'info.svg'));
-});
-
-router.get('/img/:name', (req, res, next) => {
-	if (req.params.name.endsWith('.png')) {
-		res.sendFile(path.resolve(__dirname, 'img', req.params.name))
-	} else {
-		next(httpError(404));
-	}
-});
-
-router.use((err, req, res, next) => {
-	res.send(`${err.statusCode} ${err.message}`);
-});
-
-app.use('/', router);
+app.use(router);
 
 app.listen(8000);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
-/** @type {import('webpack').Configuration} */
-module.exports = {
-	entry : './src/index.ts',
+/** @type {import('webpack').Configuration[]} */
+module.exports = [{
+	entry : './src/app/index.ts',
 	output : {
 		filename : 'app.js',
-		path : path.resolve(__dirname)
+		path : path.resolve(__dirname, 'dist')
 	},
 	module : {
 		rules : [
@@ -21,4 +21,45 @@ module.exports = {
 	},
 	mode : 'production',
 	// devtool : 'eval-source-map'
-}
+},
+{
+	entry : './bundle.js',
+	context : path.resolve(__dirname, 'src'),
+	output : {
+		filename : 'undefined.js',
+		path : path.resolve(__dirname, 'dist')
+	},
+	module : {
+		rules : [
+			{
+				test : /\.(html|css|svg|png|wav)$/,
+				use : [
+					"ignore-loader",
+					"extract-loader",
+					{
+						loader : "file-loader",
+						options : {
+							name : "[path][name].[ext]",
+							esModule : false
+						}
+					}
+				]
+			},
+			{
+				test : /\.json$/,
+				use : [
+					"ignore-loader",
+					"extract-loader",
+					{
+						loader : "file-loader",
+						options : {
+							name : "[path][name].[ext]",
+							esModule : false
+						}
+					}
+				],
+				type : "javascript/auto"
+			}
+		]
+	}
+}]
